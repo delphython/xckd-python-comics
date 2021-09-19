b970f390aebe59d52da4d43147ad62d4d4eb5eca
delphython <delphython@gmail.com>
2021-09-19 13:24:48 +0300

    Rename info to metadata

--- main.py
+++ main.py
@@ -18,15 +18,13 @@ def get_random_comic_number(url):
     response = requests.get(url)
     response.raise_for_status()
 
-    comic_info = response.json()
-    last_comic_number = comic_info["num"]
+    comic_metadata = response.json()
+    last_comic_number = comic_metadata["num"]
 
     return random.randint(1, last_comic_number)
 
-    # return
 
-
-def get_image_info(url, params=None):
+def get_image_metadata(url, params=None):
     response = requests.get(url, params)
     response.raise_for_status()
 
@@ -43,7 +41,7 @@ def download_image(url, filename, image_dir, params=None):
         file.write(response.content)
 
 
-def get_vk_upload_server_info(vk_group_id, token, token_version):
+def get_vk_upload_server_metadata(vk_group_id, token, token_version):
     vk_api_url = "https://api.vk.com/method/photos.getWallUploadServer"
 
     params = {
@@ -58,9 +56,9 @@ def get_vk_upload_server_info(vk_group_id, token, token_version):
     return response.json()["response"]
 
 
-def upload_image_to_vk(vk_upload_server_info, image_path, vk_group_id, token,
-                       token_version):
-    upload_url = vk_upload_server_info["upload_url"]
+def upload_image_to_vk(vk_upload_server_metadata, image_path, vk_group_id,
+                       token, token_version):
+    upload_url = vk_upload_server_metadata["upload_url"]
 
     with open(image_path, 'rb') as image_file:
         files = {
@@ -77,18 +75,19 @@ def upload_image_to_vk(vk_upload_server_info, image_path, vk_group_id, token,
     return response.json()
 
 
-def save_image_to_vk(upload_response_info, vk_group_id, token, token_version):
+def save_image_to_vk(upload_response_metadata, vk_group_id, token,
+                     token_version):
     vk_save_image_url = "https://api.vk.com/method/photos.saveWallPhoto"
 
-    server_to_upload = upload_response_info["server"]
-    image_info_to_upload = upload_response_info["photo"]
-    hash_to_upload = upload_response_info["hash"]
+    server_to_upload = upload_response_metadata["server"]
+    image_to_upload = upload_response_metadata["photo"]
+    hash_to_upload = upload_response_metadata["hash"]
 
     params = {
         "access_token": token,
         "v": token_version,
         "server": server_to_upload,
-        "photo": image_info_to_upload,
+        "photo": image_to_upload,
         "hash": hash_to_upload,
         "group_id": vk_group_id,
     }
@@ -99,13 +98,13 @@ def save_image_to_vk(upload_response_info, vk_group_id, token, token_version):
     return response.json()["response"][0]
 
 
-def publish_image_to_vk(save_response_info, image_comment, vk_group_id, token,
+def publish_image_to_vk(save_response_metadata, image_comment, vk_group_id, token,
                         token_version):
     vk_publish_image_url = "https://api.vk.com/method/wall.post"
     post_from_group = 1
 
-    vk_owner_id = save_response_info["owner_id"]
-    uploaded_image_id = save_response_info["id"]
+    vk_owner_id = save_response_metadata["owner_id"]
+    uploaded_image_id = save_response_metadata["id"]
     media_content = f"photo{vk_owner_id}_{uploaded_image_id}"
 
     params = {
@@ -133,35 +132,35 @@ def main():
     random_comic = get_random_comic_number(comic_url)
     random_comic_url = f"https://xkcd.com/{random_comic}/info.0.json"
 
-    image_info = get_image_info(random_comic_url)
-    image_url = image_info["img"]
-    image_comment = image_info["alt"]
+    image_metadata = get_image_metadata(random_comic_url)
+    image_url = image_metadata["img"]
+    image_comment = image_metadata["alt"]
     image_file_name = get_file_name(image_url)
     image_dir = os.getcwd()
     image_path = os.path.join(image_dir, image_file_name)
 
     download_image(image_url, image_file_name, image_dir)
 
-    vk_upload_server_info = get_vk_upload_server_info(
+    vk_upload_server_metadata = get_vk_upload_server_metadata(
         vk_group_id, vk_token,
         token_version
     )
-    vk_upload_response_info = upload_image_to_vk(
-        vk_upload_server_info,
+    vk_upload_response_metadata = upload_image_to_vk(
+        vk_upload_server_metadata,
         image_path,
         vk_group_id,
         vk_token,
         token_version
     )
-    vk_save_response_info = save_image_to_vk(
-        vk_upload_response_info,
+    vk_save_response_metadata = save_image_to_vk(
+        vk_upload_response_metadata,
         vk_group_id,
         vk_token,
         token_version
     )
 
     publish_image_to_vk(
-        vk_save_response_info,
+        vk_save_response_metadata,
         image_comment,
         vk_group_id,
         vk_token,
